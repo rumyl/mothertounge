289b17c14d8c854af6e9b7f2b98bb3e9c15181fe
Rumyl James B. Dela Cruz <rumyljames@gmail.com>
2023-11-08 12:37:40 +0800

    added register.php

---
+++ register.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Home - Mother Tongue - Based Multilingual Education</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.gstatic.com" rel="preconnect">
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/quill/quill.snow.css" rel="stylesheet">
+  <link href="assets/vendor/quill/quill.bubble.css" rel="stylesheet">
+  <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="assets/vendor/simple-datatables/style.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: NiceAdmin
+  * Updated: Sep 18 2023 with Bootstrap v5.3.2
+  * Template URL: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+
+
+  <main id="main" class="main">
+
+    <section class="section dashboard">
+      <div class="row">
+
+        <!-- Main Cover -->
+        <div class="col-lg-8">
+          <div class="row">
+
+          <!-- Card with an image on top -->
+          <div class="card">
+            <div class="card-body" style="text-align:center;">
+            <img src="assets/img/yunit1.png" style="width:200px;margin-top:20px;" class="card-img-bottom" alt="...">
+                 <h5 class="card-title" >
+                        Mother Tongue - Based<br>
+                        Multilingual Education<br>
+                        Login
+                </h5>
+
+                
+              <form method="post" action="">
+              
+              <input type="text" placeholder="Fullname" name="fullname" id="" autocomplete="off" required style="width:100%;margin:5px;border-radius:5px;"><br>
+              <input type="text" placeholder="Username" name="username" id="" autocomplete="off" required style="width:100%;margin:5px;border-radius:5px;"><br>
+              <input type="password" placeholder="Password" name="p1" id="" autocomplete="off" required style="width:100%;margin:5px;border-radius:5px;"><br>
+              <input type="password" placeholder="Confirm Password" name="p2" id="" autocomplete="off" required style="width:100%;margin:5px;border-radius:5px;"><br>
+              <div style="text-align:right">
+                <input type="submit" name="submit" value="Register" style="background:#4154f1;color:white;border-radius:10px;">
+              </div>
+              
+              </form>
+            
+            </div>
+          </div><!-- End Card with an image on top -->
+
+          </div>
+        </div>
+       
+
+      </div>
+    </section>
+
+  </main>
